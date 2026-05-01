24aeb81e1f9664c93ca7bec545321e96d4ac27b7
luizsgustavo76 <luizsgustavo76@gmail.com>
2026-05-01 19:49:17 -0300

    primeiro codigo main.cpp

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <QApplication>
+#include <QWidget>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QLineEdit>
+#include <QDebug>
+#include <functional>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QStyleFactory>
+#include <QVector>
+#include <QSplashScreen>
+#include <QStandardPaths>
+#include <QJsonDocument>
+#include <iostream>
+#include <QJsonObject>
+#include <QLabel>
+#include <QUrl>
+#include <fstream>
+#include <QEventLoop>
+#include <string>
+#include <QScrollBar>
+#include <QStackedWidget>
+#include <map>
+#include <QFile>
+#include <QDir>
+#include <QFileInfo>
+#include <QTabWidget>
+#include <QTabBar>
+#include <QMainWindow>
+#include <QJsonArray>
+#include <QScrollArea>
+#include <QTimer>
+#include <QFrame>
+#include <QIcon>
+#include <QSize>
+#include <nlohmann/json.hpp>
+#include <QPainter>
+#include <QFontMetrics>
+#include <QTranslator>
+int main(argc*, argv[]){
+    QApplication *app;
+    QMainWindow window;
+};
